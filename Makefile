36a17b4ae8b9cc8d50b4e37c3075ed7b7f42ebf8
Jakub Wilk <jwilk@jwilk.net>
2008-08-06 17:46:29 +0200

    Import ersolve 0.3.1

--- Makefile
+++ Makefile
@@ -1,18 +1,12 @@
-VERSION = 0.3
+VERSION = 0.3.1
 DISTFILES = Makefile README ersolve.c
 
-all:		ersolve
+all: ersolve
 
-ersolve:	ersolve.c
-	gcc -O2 -s ersolve.c -o ersolve
+ersolve: ersolve.c
+	$(CC) ersolve.c -o ersolve
 
 clean:
-	rm -f ersolve
+	$(RM) ersolve
 
-distclean:
-	rm -f ersolve-$(VERSION).tar.*
-        
-dist:		distclean
-	fakeroot tar cf ersolve-$(VERSION).tar $(DISTFILES)
-	bzip2 -9k ersolve-$(VERSION).tar
-	gzip -9 ersolve-$(VERSION).tar
+.PHONY: all clean
--- ersolve.c
+++ ersolve.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <netdb.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
-#define bool int
-#define true 1
-#define false 0
 
 inline void ersolve(char* str, bool verbose)
 {
@@ -17,26 +14,23 @@ inline void ersolve(char* str, bool verbose)
   struct in_addr sin_addr;
   char* ip;
   char* hostname;
-  hp=gethostbyname(str);
-  if (hp)
-  {
-    hostname=hp->h_name;
-    if (hp->h_length>(int)sizeof(sin_addr)) 
-      hp->h_length=sizeof(sin_addr);
-    memcpy(&sin_addr,hp->h_addr,hp->h_length);
-    ip=inet_ntoa(sin_addr);    
-    hp = gethostbyaddr((char*)&sin_addr.s_addr, sizeof(sin_addr.s_addr), AF_INET);
-    hostname = hp?hp->h_name:inet_ntoa(sin_addr);
-    printf("%s == %s",hostname,ip);
-    if (verbose)
-      printf(" ( %s )",str);
-    printf("\n");
-  }
-  else
+  hp = gethostbyname(str);
+  if (!hp)
   {
     printf("%s [!]\n",str);
     return;
   }
+  hostname = hp->h_name;
+  if (hp->h_length > sizeof sin_addr) 
+    hp->h_length = sizeof sin_addr;
+  memcpy(&sin_addr, hp->h_addr, hp->h_length);
+  ip = inet_ntoa(sin_addr);
+  hp = gethostbyaddr((char*)&sin_addr.s_addr, sizeof sin_addr.s_addr, AF_INET);
+  hostname = hp ? hp->h_name : inet_ntoa(sin_addr);
+  printf("%s == %s", hostname, ip);
+  if (verbose)
+    printf(" ( %s )", str);
+  printf("\n");
 }
 
 void usage(void)
@@ -48,21 +42,21 @@ void usage(void)
 int main(int argc, char** argv)
 {
   bool verbose;
-  if (argc<=1)
+  if (argc <= 1)
     usage();
   argv++;
-  if (!strcmp(*argv,"-v"))
+  if (strcmp(*argv, "-v") == 0)
   {
-    verbose=true;
+    verbose = true;
     argv++;
   }
   else
-    verbose=false;
-  if (!*argv)
-    usage();
-  while(*argv)
+    verbose = false;
+  while (*argv != NULL)
   {
-    ersolve(*argv,verbose);
+    ersolve(*argv, verbose);
     argv++;
   }
 }
+
+/* vim:set ts=2 sw=2 et:*/
